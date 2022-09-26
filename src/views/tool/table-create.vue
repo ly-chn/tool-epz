64f17c93b607c1a3ecb6511313eb835371949c8b
xy <3293232930@qq.com>
2022-09-26 18:25:29 +0800

    哈哈, 哈哈

--- src/views/tool/table-create.vue
+++ src/views/tool/table-create.vue
@@ -4,11 +4,40 @@
     cols: <input v-model="cols" type="number">
     <div>
       <table>
-        <tr v-for="(row, rowIndex) in cellMap" :key="rowIndex">
-          <th v-for="(cell, colIndex) in row" :key="colIndex">
-            <table-cell v-model="cellMap[rowIndex][colIndex]"/>
-            <!--hi this is text-->
-          </th>
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td>Mexico</td>
+        </tr>
+        <tr>
+          <td>Ernst Handel</td>
+          <td>Roland Mendel</td>
+          <td>Austria</td>
+        </tr>
+        <tr>
+          <td>Island Trading</td>
+          <td>Helen Bennett</td>
+          <td>UK</td>
+        </tr>
+        <tr>
+          <td>Laughing Bacchus Winecellars</td>
+          <td>Yoshi Tannamuri</td>
+          <td>Canada</td>
+        </tr>
+        <tr>
+          <td>Magazzini Alimentari Riuniti</td>
+          <td>Giovanni Rovelli</td>
+          <td>Italy</td>
         </tr>
       </table>
     </div>
@@ -21,6 +50,7 @@ import TableCell from '/@/views/tool/component/table-cell.vue'
 
 const rows = ref(10)
 const cols = ref(10)
+const borderRadius = ref(8)
 
 const cellMap = ref([['你好']])
 watchEffect(() => {
@@ -28,27 +58,26 @@ watchEffect(() => {
 })
 </script>
 <style scoped>
-th{
-  padding: 2px
-}
 table {
+  border-collapse: collapse;
   width: 100%;
-  background-color: yellowgreen;
-  text-align: center;
-  border-collapse: separate;
-  border-spacing: 0;
-  border-radius: 10px;
-  border: solid 2px #1E90FF;
+  border-radius: 20px;
 }
+
+table::after {
+  content: " ";
+  inset: 0;
+  border: solid #000 1px;
+  position: absolute;
+  border-radius: 20px;
+}
+tr:not(:last-child) {
+  border-bottom: solid #000 1px
+}
+
 td, th {
-  border: 2px solid #1E90FF;
-  height: 30px;
-  text-align: center;
-}
-td {
-  border-top: 2px solid #1E90FF;
-}
-td:last-child, th:last-child {
-  border-right-color: transparent;
+
+  text-align: left;
+  padding: 8px;
 }
 </style>
